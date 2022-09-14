94f78a54038e5601f5ddac70f2e59d141f01fc5d
chjameskumar <112061903+chjameskumar@users.noreply.github.com>
2022-09-14 22:31:53 +0530

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -2,72 +2,94 @@ import re
 
 def email():
     while True:
-        userid = input(' Enter UserId/email : ')
-        if len(userid) >= 6:
-            if userid[0].isalpha():
-                if "@" in userid and userid.count("@") == 1:
-                    if userid[-5] == "@" or userid[-4] == "@":
-                        print(' Enter Valid email/UserId : ')
-                        continue
-                    else:
-                        if userid[-3] == "." or userid[-4] == ".":
-                            return userid
-        print(' Enter Valid email/UserId : ')
+        UserId = input('Enter Email/UserId : ')
+        if UserId[0].isalpha():
+            if '@' in UserId and UserId.count('@') == 1:
+                if UserId.endswith('.com') or UserId.endswith('.in'):
+                    if UserId.index('@')+1 != UserId.rfind('.'):
+                        return UserId
+        print('Enter correct Email/UserId!!! ')
         continue
 
 def passwd():
     while True:
-        password = input(' Enter your password : ')
+        password = input('Enter Password :')
         if len(password) > 5 and len(password) < 16:
-            if re.search('[$%&!@#]', password):
-                if re.search('[a-z]', password):
-                    if re.search('[A-Z]', password):
-                        if re.search('[0-9]', password):
+            if re.search('[a-z]', password):
+                if re.search('[A-Z]', password):
+                    if re.search('[0-9]', password):
+                        if re.search('[!@#$%&*~]', password):
                             return password
-        print(' Enter valid password : ')
+        print('Enter valid password!!! ')
         continue
+def data():
+    global user_data
+    global passwd_data
+    user_data = []
+    passwd_data = []
 
-def data(List):
-  option = input(''' To Register press 1 \n To Login press 2 \n To Exit press 3  ''')
-  if (option != '1' and option != '2' and option != '3'):
-    data()
-  if option == '1':
+def register():
     UserId = email()
     password = passwd()
-    L1 = [UserId,password]
-    List.append(L1)
-    print('Registered successfully!!!')
-    data(List)
-  elif option == '2':
-    UserId = email()
-    for i in List:
-      if UserId == i[0]:
-        password=passwd()
-        if password == i[1]:
-          print('Logged in successfully!!!')
-          break
-        else:
-          print('wrong password!')
-          ch = input('If you want to change the password press 1 ')
-          if ch == '1':
-            password=passwd()
-            i[1]=password
-          else:
-            data(List)
-      else:
-        print('UserId/email does not exist.')
-        print('Please do registration first to Log in')
-        op = input('To register press 1 ')
-        if op == 1:
-          UserId = email()
-          password = passwd()
-          List = List.append([UserId,password])
-          print('Registered successfully!!!')
-        else:
-          data(List)
-    data(List)
-  elif option == '3':
-    print('Thank You!!!')
+    user_data.append(UserId)
+    passwd_data.append(password)
+    print('You have successfully Registered...')
+    op()
 
-List = []
-data(List)
+def login():
+    global j
+    UserId = email()
+    for i in user_data:
+        if i == UserId:
+            j = i
+            password = passwd()
+            if password == passwd_data[user_data.index(i)]:
+                print('You have successfully Logged in...')
+                op()
+            else:
+                print('You have entered wrong password!!!')
+                reset()
+        else:
+            continue
+    print("Your credentials doesn't exist!!!")
+    op()
+
+def reset():
+    choice = input('''Choose an option :
+    To change the password  Press 1 
+    To go to Main menu      Press 2 
+    To Exit                 Press 3''')
+    if choice == '1':
+        print('Please enter new password.')
+        password = passwd()
+        passwd_data[user_data.index[j]] = password
+        print('You have successfully changed the password...')
+        op()
+    elif choice == '2':
+        op()
+    elif choice == '3':
+        exit()
+    else:
+        print('Enter number 1/2/3 only...')
+        reset()
+
+def op():
+    option = input('''Please choose any One :
+    For Registration   press 1 
+    For Login          press 2 
+    To Exit            press 3 ''')
+    if option == '1':
+        register()
+    elif option == '2':
+        login()
+    elif option == '3':
+        exit()
+    else:
+        print('Enter number 1/2/3 only...')
+        op()
+
+def exit():
+    print('Thank you!!!')
+
+data()
+op()
